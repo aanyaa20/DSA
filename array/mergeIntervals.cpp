fef76fedbc8cc76f320cbcc280ade1b7faa4b214
aanya.singhal20@gmail.com <aanya.singhal20@gmail.com>
2025-09-20 09:49:12 +0530

    Merge intervals added

---
+++ array/mergeIntervals.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+#include<vector>
+ class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        for(int i=0; i<nums.size() ; i++)
+        {
+            if(ans.empty() || nums[i][0]> ans.back()[1])
+            {
+                ans.push_back(nums[i]);
+            }
+            else
+            {
+                ans.back()[1]= max(ans.back()[1],nums[i][1]);
+            }
+        }
+        return ans;
+        
+    }
+};
